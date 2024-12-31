ca37143c42b8373e9e6357795950cd8999fd33f1
Lumikrynd <WhatIsEmail?>
2025-01-01 00:18:58 +0100

    dk keycodes

--- keyboards/splitkb/kyria/rev3/keymaps/lumikrynd/keymap.c
+++ keyboards/splitkb/kyria/rev3/keymaps/lumikrynd/keymap.c
@@ -15,6 +15,10 @@
  */
 #include QMK_KEYBOARD_H
 
+#include "keymap_danish.h" //see qmk_firmware/quantum/keymap_extras/keymap_danish.h
+#define DK_OE DK_OSTR
+#define DK_AA DK_ARNG
+
 enum layers {
     _QWERTY = 0,
     _NAV,
@@ -33,8 +37,7 @@ enum layers {
 #define ADJUST   MO(_ADJUST)
 
 #define CTL_ESC  MT(MOD_LCTL, KC_ESC)
-#define CTL_QUOT MT(MOD_RCTL, KC_QUOTE)
-#define CTL_MINS MT(MOD_RCTL, KC_MINUS)
+#define CTL_DIA  MT(MOD_RCTL, DK_DIAE)
 #define ALT_ENT  MT(MOD_LALT, KC_ENT)
 
 // Note: LAlt/Enter (ALT_ENT) is not the same thing as the keyboard shortcut Alt+Enter.
@@ -58,9 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                        `----------------------------------'  `----------------------------------'
  */
     [_QWERTY] = LAYOUT(
-     KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
-     CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+     KC_TAB  , DK_Q ,  DK_W   ,  DK_E  ,   DK_R ,   DK_T ,                                        DK_Y,   DK_U ,  DK_I ,   DK_O ,  DK_P , KC_BSPC,
+     CTL_ESC , DK_A ,  DK_S   ,  DK_D  ,   DK_F ,   DK_G ,                                        DK_H,   DK_J ,  DK_K ,   DK_L ,DK_QUOT, CTL_DIA,
+     KC_LSFT , DK_Z ,  DK_X   ,  DK_C  ,   DK_V ,   DK_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, DK_N,   DK_M ,DK_COMM, DK_DOT ,DK_MINS, KC_RSFT,
                                 ADJUST , KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_APP
     ),
 
