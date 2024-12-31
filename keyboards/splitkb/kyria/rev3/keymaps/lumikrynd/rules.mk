f3d005592c92af63c48bdf0d481f78250e0a5c51
Lumikrynd <WhatIsEmail?>
2024-12-31 22:14:20 +0100

    adding my layout

---
+++ keyboards/splitkb/kyria/rev3/keymaps/lumikrynd/rules.mk
@@ -0,0 +1,4 @@
+OLED_ENABLE = yes
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
