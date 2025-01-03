c8ad41a9c71950833125b68ebafd0ab1359a308e
Lumikrynd <WhatIsEmail?>
2025-01-03 22:57:20 +0100

    Mouse configurations

--- keyboards/splitkb/kyria/rev3/keymaps/lumikrynd/config.h
+++ keyboards/splitkb/kyria/rev3/keymaps/lumikrynd/config.h
@@ -19,6 +19,18 @@
 #define TAPPING_TERM 200
 #define COMBO_TERM 35
 
+// Mouse configs
+#define MOUSEKEY_DELAY 10               // Delay between pressing a movement key and cursor movement
+#define MOUSEKEY_INTERVAL 20            // Time between cursor movements in milliseconds
+#define MOUSEKEY_MOVE_DELTA 5           // Step size
+#define MOUSEKEY_MAX_SPEED 8            // Maximum cursor speed at which acceleration stops
+#define MOUSEKEY_TIME_TO_MAX 60         // Time until maximum cursor speed is reached
+#define MOUSEKEY_WHEEL_DELAY 10         // Delay between pressing a wheel key and wheel movement
+#define MOUSEKEY_WHEEL_INTERVAL 80      // Time between wheel movements
+#define MOUSEKEY_WHEEL_DELTA 1          // Wheel movement step size
+#define MOUSEKEY_WHEEL_MAX_SPEED 8      // Maximum number of scroll steps per scroll action
+#define MOUSEKEY_WHEEL_TIME_TO_MAX 40   // Time until maximum scroll speed is reached
+
 #ifdef RGBLIGHT_ENABLE
 #    define RGBLIGHT_EFFECT_BREATHING
 #    define RGBLIGHT_EFFECT_RAINBOW_MOOD
--- keyboards/splitkb/kyria/rev3/keymaps/lumikrynd/rules.mk
+++ keyboards/splitkb/kyria/rev3/keymaps/lumikrynd/rules.mk
@@ -3,3 +3,4 @@ ENCODER_ENABLE = no        # Disables encoders for now
 RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
 COMBO_ENABLE = yes
+MOUSEKEY_ENABLE = yes
