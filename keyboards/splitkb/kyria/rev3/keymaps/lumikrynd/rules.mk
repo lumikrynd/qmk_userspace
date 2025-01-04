3b8f1ca2b622d3eaf38d6fddd27cba031a112759
Lumikrynd <WhatIsEmail?>
2025-01-04 21:24:42 +0100

    Disable RGB's, seems to cause problems

--- keyboards/splitkb/kyria/rev3/keymaps/lumikrynd/rules.mk
+++ keyboards/splitkb/kyria/rev3/keymaps/lumikrynd/rules.mk
@@ -1,6 +1,6 @@
 OLED_ENABLE = yes
-ENCODER_ENABLE = no        # Disables encoders for now
-RGB_MATRIX_ENABLE = no     # Disable keyboard RGB matrix
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+ENCODER_ENABLE = no
+RGB_MATRIX_ENABLE = no     # RGB matrix
+RGBLIGHT_ENABLE = no       # RGB underglow
 COMBO_ENABLE = yes
 MOUSEKEY_ENABLE = yes
